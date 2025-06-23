3e5a21a02f178049c3c67e54ab8c796a61dce260
wallee-deployment-user <technical-systems@wallee.com>
2025-06-23 17:12:53 +0200

    Documentation for Release version: 10.0.0

--- Package.swift
+++ Package.swift
@@ -3,19 +3,15 @@ import PackageDescription
 
 let package = Package(
     name: "WalleeTestSdk",
-    platforms: [.iOS("13.0")],
+    platforms: [.iOS("12.4")],
     products: [
         .library(name: "WalleeTestSdk",
-                 targets: ["TwintSDK","WalleeTestSdk","PaymentResources","ThreeDS_SDK"]),
+                 targets: ["ThreeDS_SDK","TwintSDK","WalleeTestSdk","PaymentResources"]),
     ],
     targets: [
+		.binaryTarget(name: "ThreeDS_SDK", path: "ThreeDS_SDK.xcframework"),
 		.binaryTarget(name: "TwintSDK", path: "TwintSDK.xcframework"),
 		.binaryTarget(name: "WalleeTestSdk", path: "WalleeTestSdk.xcframework"),
-		.binaryTarget(
-			name: "ThreeDS_SDK",
-			url: "https://merchant-plug-in.extranet.netcetera.biz/nca-348-1/readonly-dav/3dssdk-release/ios/release/2.5.22/ThreeDS_SDK.zip",
-			checksum: "8ed06e2a903cd04c98fb450a2a44788f18aeaa3f8a108ffe795b9526e34248e6"
-		),
 	.target(
 		name: "PaymentResources",
 		dependencies: [
